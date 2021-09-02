3ff3781f8be2c479cc715f812cbaa20e529c8bac
Isa SUMER <sumerisa5@gmail.com>
2021-09-02 12:28:29 +0300

    axios and api added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 
-function App() {
+const App = () => {
+  const [itemList, setItemList] = useState([]);
+
+  // useEffect(()=>{
+  //   axios.get("https://randomuser.me/api/").then((res)=>setItemList(res.data.results))
+  // },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="App">
+
+
+</div>
+  )
 }
 
 export default App;
+
+
