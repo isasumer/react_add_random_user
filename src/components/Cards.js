0db467fea69e45b69c81fef89b0139b8c7fe3b59
Isa SUMER <sumerisa5@gmail.com>
2021-09-03 03:09:04 +0300

    switch case added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import "./Cards.css";
-
 import man from "../assets/man.svg";
 import mail from "../assets/mail.svg";
 import woman from "../assets/woman.svg";
@@ -10,25 +9,78 @@ import phone from "../assets/phone.svg"
 import gwoman from "../assets/growing-up-woman.svg"
 import map from "../assets/map.png"
 import axios from "axios";
+// import { is } from "@babel/types";
 
 const Cards = () => {
+    // const [isVisible, setVisible] = useState(true)
+    // const [itemList, setItemList] = useState([])
+    const [itemList, setItemList] = useState([]);
+    const [isVisible, setVisible] = useState(true);
+
+    useEffect (async ()=> {
+       await axios.get("https://randomuser.me/api/").then((res)=>setItemList(res.data.results[0]))
+       .catch((err)=>console.log(err))
+        
+    },[isVisible])
+//    const itemList1 = Object.keys(itemList).map((key) => {
+//         return key= {key}
+//     } )
+
+function showText(txt) {
+    const text = "My"+ txt+  "is";
+    document.querySelector(".info").innerHTML=text;
+    
+    switch (txt.trim()) {
+        case "name":
+            const name=true;
+            document.querySelector(".isa").innerHTML=<span> name || "Clarusway"</span>;
+            console.log(name);
+          break;
+        case "email":
+            document.querySelector(".my").innerHTML=itemList.email || "Clarusway";
+          break;
+        case "age":
+            document.querySelector(".my").innerHTML=itemList.dob?.age || "Clarusway";
+          break;
+        case "city":
+            document.querySelector(".my").innerHTML=itemList.location?.city || "Clarusway";
+          break;
+        case "phone":
+            document.querySelector(".my").innerHTML=itemList?.phone || "Clarusway";
+          break;
+        case "password":
+            document.querySelector(".my").innerHTML=itemList.login?.password || "Clarusway";
+          break;
+        
+      }
+    document.querySelector(".my").innerHTML=itemList.info;
+    
+}
   return (
+    
     <div className="Cards">
-      <img id="profil_photo"  />
+      <img id="profil_photo" src={itemList.picture?.large || null } alt="photo" />
       <div className="infos">
-          <h4 className="my">My {} is</h4>
-          <h3 className="info"></h3>
+          <h4 className="info"></h4>
+          <h3 className="isa"></h3>
+          
       </div>
       <div className="icons">
-        <img className="icon" src={true ? man: woman} alt="" />
-        <img className="icon" src={mail} alt="" />
-        <img className="icon" src={gwoman} alt="" />
-        <img className="icon" src={map} alt="" />
-        <img className="icon" src={phone} alt="" />
-        <img className="icon" src={padlock} alt="" />
+        <img className="icon" src={true ? man: woman} alt="name" onMouseOver={()=>
+                        showText(" name ")}/>
+        <img className="icon" src={mail} alt="mail" onMouseOver={()=>
+                        showText(" email ")} />
+        <img className="icon" src={gwoman} alt="gwoman" onMouseOver={()=>
+                        showText(" age ")}/>
+        <img className="icon" src={map} alt="map" onMouseOver={()=>
+                        showText(" city ")} />
+        <img className="icon" src={phone} alt="phone" onMouseOver={()=>
+                        showText(" phone " )} />
+        <img className="icon" src={padlock} alt="padlock" onMouseOver={()=>
+                        showText(" password ")}/>
       </div>
       <div className="btn-group">
-          <button className="btn" >New User</button>
+      <button type="button" className="newUser" onClick={()=>{setVisible(!isVisible)}}>New User</button>
           <button className="btn">Add User</button>
       </div>
     </div>
