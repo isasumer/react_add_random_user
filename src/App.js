487d993b89fd2de82f386868ec3399c3108119e6
Isa SUMER <sumerisa5@gmail.com>
2021-09-07 14:26:32 +0300

    react add random user project finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import "./App.css"
-import cw from "./assets/cw.svg"
-
+import "./App.css";
+import cw from "./assets/cw.svg";
+import design from "./assets/design.svg";
 import Cards from "./components/Cards";
 
 const App = () => {
-  
+  const word = "<eden8/>"; //just used at the bottom
+
   return (
-<div className="App">
-    <Logo/>
-    <Cards  />
-</div>
-  )
-}
+    <div className="App">
+      <div>
+        <img id="cw_logo" src={cw}></img>
+      </div>
+      <Cards />
+      <div className="bottom">
+        <p id="ed8en">{word}</p>
+        <img id="design_logo" src={design} />
+        <p id="design">design</p>
+      </div>
+    </div>
+  );
+};
 export default App;
-
-
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,4 @@
 import "./Cards.css";
-import design from "../assets/design.svg";
 import React, { useState, useEffect } from "react";
 import man from "../assets/man.svg";
 import mail from "../assets/mail.svg";
@@ -12,11 +11,14 @@ import map from "../assets/map.png";
 import axios from "axios";
 
 const Cards = () => {
-  const word = "<eden8/>";
   const [itemList, setItemList] = useState([]);
   const [newUser, setNewUser] = useState(true);
   const [showTable, setShowTable] = useState(false);
-  const [showText, setShowText] = useState({show:true, upper: "", bottom: "" });
+  const [showText, setShowText] = useState({
+    show: true,
+    upper: "",
+    bottom: "",
+  });
   const [addButtonList, setAddButtonList] = useState([]);
 
   useEffect(async () => {
@@ -38,118 +40,124 @@ const Cards = () => {
   };
   return (
     <div className="Cards">
-      <div className="photo_box">
-        <img
-          id="profil_photo"
-          src={itemList[0]?.picture?.large || null}
-          alt="photo"
-        />
+      <div className="Card">
+        <div className="photo_box">
+          <img
+            id="profil_photo"
+            src={itemList[0]?.picture?.large || null}
+            alt="photo"
+          />
+        </div>
+        <div className="infos">
+          {showText.show}
+          <h4 className="info">{showText.upper}</h4>
+          <h3 className="isa">{showText.bottom}</h3>
+        </div>
+        <div className="icons">
+          <img
+            className="icon"
+            src={itemList[0]?.gender == "male" ? man : woman}
+            alt="name"
+            onMouseOver={() =>
+              setShowText({
+                show: true,
+                upper: "My name is",
+                bottom: itemList[0]?.name?.first,
+              })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+          <img
+            className="icon"
+            src={mail}
+            alt="mail"
+            onMouseOver={() =>
+              setShowText({ upper: "My e-mail is", bottom: itemList[0]?.email })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+          <img
+            className="icon"
+            src={itemList[0]?.gender == "male" ? gman : gwoman}
+            alt="gwoman-gman"
+            onMouseOver={() =>
+              setShowText({ upper: "My age is", bottom: itemList[0]?.dob?.age })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+          <img
+            className="icon"
+            src={map}
+            alt="map"
+            onMouseOver={() =>
+              setShowText({
+                upper: "My city is",
+                bottom:
+                  itemList[0]?.location?.city +
+                  ", " +
+                  itemList[0]?.location?.country,
+              })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+          <img
+            className="icon"
+            src={phone}
+            alt="phone"
+            onMouseOver={() =>
+              setShowText({ upper: "My phone is", bottom: itemList[0]?.cell })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+          <img
+            className="icon"
+            src={padlock}
+            alt="padlock"
+            onMouseOver={() =>
+              setShowText({
+                upper: "My password is",
+                bottom: itemList[0]?.login?.password,
+              })
+            }
+            onMouseOut={() =>
+              setShowText({ show: false, upper: "", bottom: "" })
+            }
+          />
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn newUser"
+            onClick={() => {
+              setNewUser(!newUser);
+            }}
+          >
+            New User
+          </button>
+          <button
+            type="button"
+            className="btn addUser"
+            onClick={() => {
+              addBtn();
+              setShowTable(true);
+            }}
+          >
+            Add User
+          </button>
+        </div>
       </div>
-      <div className="infos">
-        {showText.show}
-        <h4 className="info">{showText.upper}</h4>
-        <h3 className="isa">{showText.bottom }</h3>
-      </div>
-      <div className="icons">
-        <img
-          className="icon"
-          src={itemList[0]?.gender == "male" ? man : woman}
-          alt="name"
-          onMouseOver={() =>
-            setShowText({show:true, upper: "My name is", bottom: itemList[0]?.name?.first })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-        <img
-          className="icon"
-          src={mail}
-          alt="mail"
-          onMouseOver={() =>
-            setShowText({ upper: "My e-mail is", bottom: itemList[0]?.email })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-        <img
-          className="icon"
-          src={itemList[0]?.gender == "male" ? gman : gwoman}
-          alt="gwoman-gman"
-          onMouseOver={() =>
-            setShowText({ upper: "My age is", bottom: itemList[0]?.dob?.age })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-        <img
-          className="icon"
-          src={map}
-          alt="map"
-          onMouseOver={() =>
-            setShowText({
-              upper: "My city is",
-              bottom:
-                itemList[0]?.location?.city +
-                ", " +
-                itemList[0]?.location?.country,
-            })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-        <img
-          className="icon"
-          src={phone}
-          alt="phone"
-          onMouseOver={() =>
-            setShowText({ upper: "My phone is", bottom: itemList[0]?.cell })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-        <img
-          className="icon"
-          src={padlock}
-          alt="padlock"
-          onMouseOver={() =>
-            setShowText({
-              upper: "My password is",
-              bottom: itemList[0]?.login?.password,
-            })
-          }
-          onMouseOut={() =>
-            setShowText({show:false, upper:"", bottom:"" })
-          }
-        />
-      </div>
-      <div className="btn-group">
-        <button
-          type="button"
-          className="btn newUser"
-          onClick={() => {
-            setNewUser(!newUser);
-          }}
-        >
-          New User
-        </button>
-        <button
-          type="button"
-          className="btn addUser"
-          onClick={() => {
-            addBtn();
-            setShowTable(true);
-          }}
-        >
-        Add User
-        </button>
-      </div>
-      <div>
-        <table className="addUserList">
+      <div className="addUserList">
+        <table>
           {showTable ? (
             <thead>
               <tr className="userListTable">
@@ -171,11 +179,6 @@ const Cards = () => {
             ))}
           </tbody>
         </table>
-      </div>
-      <div className="bottom">
-        <p id="ed8en">{word}</p>
-        <img id="design_logo" src={design} />
-        <p id="design">design</p>
       </div>
     </div>
   );
